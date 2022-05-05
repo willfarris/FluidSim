dced43370b2e06d64d96e4cb41ff84580e158422
Will Farris <9190155+WillFarris@users.noreply.github.com>
2022-05-04 19:32:56 -0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate sdl2;
 
 
+use std::ops::Bound;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
@@ -11,237 +13,231 @@ const GRID_SIZE: u32 = 8;
 const NUM_SQUARES: usize = (WINDOW_WIDTH/GRID_SIZE) as usize;
 const ARRAY_SIZE: usize = (NUM_SQUARES+2)*(NUM_SQUARES+2);
 
-fn idx(field: &[f32; ARRAY_SIZE], x: usize, y: usize) -> f32 {
-    field[x + (NUM_SQUARES+2)*y]
+struct Fluid {
+    visc: f32,
+
+    s: [f32; ARRAY_SIZE],
+
+    d: [f32; ARRAY_SIZE],
+    u: [f32; ARRAY_SIZE],
+    v: [f32; ARRAY_SIZE],
+
+    d0: [f32; ARRAY_SIZE],
+    u0: [f32; ARRAY_SIZE],
+    v0: [f32; ARRAY_SIZE],
+
+    _div: [f32; ARRAY_SIZE],
+    _p: [f32; ARRAY_SIZE],
 }
 
-fn set_idx(field: &mut [f32; ARRAY_SIZE], val: f32, x: usize, y: usize) {
-    field[x + (NUM_SQUARES+2)*y] = val;
+trait FluidArray<T> {
+    fn idx(&self, x: usize, y: usize) -> T;
+    fn set_idx(&mut self, val: T, x: usize, y: usize);
+    fn set_bnd(&mut self, b: u32);
+    fn diffuse(&mut self, b: u32, x0: &[T; ARRAY_SIZE], diff: T, dt: f32);
+    fn advect(&mut self, b: u32, d0: &[T; ARRAY_SIZE], u: &[T; ARRAY_SIZE], v: &[T; ARRAY_SIZE], dt: f32);
+    //fn project(u: &mut [T; ARRAY_SIZE], v: &mut [T; ARRAY_SIZE], p: &mut [T; ARRAY_SIZE], div: &mut [T; ARRAY_SIZE]) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE]);
 }
-    
-fn set_bnd(b: u32, field: &mut [f32; ARRAY_SIZE]) {
-    for i in 1..=NUM_SQUARES {
-        set_idx(field, if b == 1 {-idx(field, 1, i)} else {idx(field, 1, i)}, 0, i);
-        set_idx(field, if b == 1 {-idx(field, NUM_SQUARES, i)} else {idx(field, NUM_SQUARES, i)}, NUM_SQUARES+1, i);
-        set_idx(field, if b == 2 {-idx(field, i, 1)} else {idx(field, i, 1)}, i, 0);
-        set_idx(field, if b == 2 {-idx(field, i, NUM_SQUARES)} else {idx(field, i, NUM_SQUARES)}, i, NUM_SQUARES+1);
+
+impl FluidArray<f32> for [f32; ARRAY_SIZE] {
+    fn idx(&self, x: usize, y: usize) -> f32 {
+        self[x + (NUM_SQUARES+2)*y]
     }
-    set_idx(field, 0.5*(idx(field, 1, 0)+idx(field, 0, 1)), 0, 0);
 
-    set_idx(field, 0.5*(idx(field, 1, NUM_SQUARES+1)+idx(field, 0, NUM_SQUARES)), 0, NUM_SQUARES+1);
-    
-    set_idx(field, 0.5*(idx(field, NUM_SQUARES, 0)+idx(field, NUM_SQUARES+1, 1)), NUM_SQUARES+1, 0);
-    
-    set_idx(field, 0.5*(idx(field, NUM_SQUARES, NUM_SQUARES+1)+idx(field, NUM_SQUARES+1, NUM_SQUARES)), NUM_SQUARES+1, NUM_SQUARES+1);
-}
+    fn set_idx(&mut self, val: f32, x: usize, y: usize) {
+        self[x + (NUM_SQUARES+2)*y] = val;
+    }
 
-fn diffuse(b: u32, mut x: [f32; ARRAY_SIZE], x0: [f32; ARRAY_SIZE], diff: f32, dt: f32) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
-    let a = dt * diff * (NUM_SQUARES*NUM_SQUARES) as f32;
-
-    for _ in 0..20 {
+    fn set_bnd(&mut self, b: u32) {
         for i in 1..=NUM_SQUARES {
-            for j in 1..=NUM_SQUARES {
-                let val = (idx(&x0, i, j) + a * (idx(&x, i-1, j) + idx(&x, i+1, j) + idx(&x, i, j - 1) + idx(&x, i, j + 1))) / (1. + 4.*a);
-                set_idx(&mut x, val, i, j);
-            }
+            self.set_idx( if b == 1 {-self.idx( 1, i)} else {self.idx( 1, i)}, 0, i);
+            self.set_idx(if b == 1 {-self.idx(NUM_SQUARES, i)} else {self.idx(NUM_SQUARES, i)}, NUM_SQUARES+1, i);
+            self.set_idx(if b == 2 {-self.idx(i, 1)} else {self.idx(i, 1)}, i, 0);
+            self.set_idx(if b == 2 {-self.idx(i, NUM_SQUARES)} else {self.idx(i, NUM_SQUARES)}, i, NUM_SQUARES+1);
         }
-        set_bnd(b, &mut x);
+        self.set_idx(0.5*(self.idx(1, 0)+self.idx(0, 1)), 0, 0);
+        self.set_idx(0.5*(self.idx(1, NUM_SQUARES+1)+self.idx(0, NUM_SQUARES)), 0, NUM_SQUARES+1);
+        self.set_idx(0.5*(self.idx(NUM_SQUARES, 0)+self.idx(NUM_SQUARES+1, 1)), NUM_SQUARES+1, 0);
+        self.set_idx(0.5*(self.idx(NUM_SQUARES, NUM_SQUARES+1)+self.idx(NUM_SQUARES+1, NUM_SQUARES)), NUM_SQUARES+1, NUM_SQUARES+1);
     }
 
-    (x, x0)
-}
-
-fn advect(b: u32, mut d: [f32; ARRAY_SIZE], d0: [f32; ARRAY_SIZE], u: &[f32; ARRAY_SIZE], v: &[f32; ARRAY_SIZE], dt: f32) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
-
-    let dt0 = dt*NUM_SQUARES as f32;
-    for i in 1..NUM_SQUARES {
-        for j in 1..NUM_SQUARES {
-            let mut x = i as f32 - dt0*idx(&u, i, j);
-            let mut y = j as f32 - dt0*idx(&v, i, j);
-
-            if x < 0.5 {
-                x = 0.5;
+    fn diffuse(&mut self, b: u32, x0: &[f32; ARRAY_SIZE], diff: f32, dt: f32) {
+        let a = dt * diff * (NUM_SQUARES*NUM_SQUARES) as f32;
+    
+        for _ in 0..20 {
+            for i in 1..=NUM_SQUARES {
+                for j in 1..=NUM_SQUARES {
+                    let val = (x0.idx(i, j) + a * (self.idx(i-1, j) + self.idx(i+1, j) + self.idx(i, j - 1) + self.idx(i, j + 1))) / (1. + 4.*a);
+                    self.set_idx( val, i, j);
+                }
             }
-            if x > NUM_SQUARES as f32 + 0.5 {
-                x = NUM_SQUARES as f32 + 0.5;
-            }
-            let i0 = x.floor() as usize;
-            let i1 = i0 + 1;
-
-            if y < 0.5 {
-                y = 0.5;
-            }
-            if y > NUM_SQUARES as f32 + 0.5 {
-                y = NUM_SQUARES as f32 + 0.5;
-            }
-            let j0 = y.floor() as usize;
-            let j1 = j0 + 1;
-
-            let s1 = x - i0 as f32;
-            let s0 = 1. - s1;
-
-            let t1 = y - j0 as f32;
-            let t0 = 1. - t1;
-
-            let d_new = s0*(t0*idx(&d0, i0, j0) + t1*idx(&d0, i0, j1)) + s1*(t0*idx(&d0, i1, j0) + t1*idx(&d0, i1, j1));
-            set_idx(&mut d, d_new, i, j);
+            self.set_bnd(b,);
         }
     }
-    set_bnd(b, &mut d);
-    (d, d0)
-}
 
-fn project(mut u: [f32; ARRAY_SIZE], mut v: [f32; ARRAY_SIZE]) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
-    let h = 1./NUM_SQUARES as f32;
-    let mut div = [0f32; ARRAY_SIZE];
-    let mut p = [0f32; ARRAY_SIZE];
+    fn advect(&mut self, b: u32, d0: &[f32; ARRAY_SIZE], u: &[f32; ARRAY_SIZE], v: &[f32; ARRAY_SIZE], dt: f32) {
 
-    for i in 1..=NUM_SQUARES {
-        for j in 1..=NUM_SQUARES {
-            let div_val = -0.5 * h * (idx(&u, i+1, j) - idx(&u, i-1, j) + idx(&v, i, j+1) - idx(&v, i, j-1));
-            set_idx(&mut div, div_val, i, j);
-        }
-    }
-    set_bnd(0, &mut div);
-    set_bnd(0, &mut p);
+        let dt0 = dt*NUM_SQUARES as f32;
 
-    for _ in 0..20 {
         for i in 1..NUM_SQUARES {
             for j in 1..NUM_SQUARES {
-                let p_val = (idx(&div, i, j) + idx(&p, i-1, j) + idx(&p, i+1, j) + idx(&p, i, j-1) + idx(&p, i, j+1))/4.;
-                set_idx(&mut p, p_val, i, j);
+                let mut x = i as f32 - dt0*u.idx(i, j);
+                let mut y = j as f32 - dt0*v.idx(i, j);
+    
+                if x < 0.5 {
+                    x = 0.5;
+                }
+                if x > NUM_SQUARES as f32 + 0.5 {
+                    x = NUM_SQUARES as f32 + 0.5;
+                }
+                let i0 = x.floor() as usize;
+                let i1 = i0 + 1;
+    
+                if y < 0.5 {
+                    y = 0.5;
+                }
+                if y > NUM_SQUARES as f32 + 0.5 {
+                    y = NUM_SQUARES as f32 + 0.5;
+                }
+                let j0 = y.floor() as usize;
+                let j1 = j0 + 1;
+    
+                let s1 = x - i0 as f32;
+                let s0 = 1. - s1;
+    
+                let t1 = y - j0 as f32;
+                let t0 = 1. - t1;
+    
+                let d_new = s0*(t0*d0.idx(i0, j0) + t1*d0.idx(i0, j1)) + s1*(t0*d0.idx(i1, j0) + t1*d0.idx(i1, j1));
+                self.set_idx(d_new, i, j);
             }
         }
-        set_bnd(0, &mut p);
+        self.set_bnd(b);
     }
+}
 
-    for i in 1..NUM_SQUARES {
-        for j in 1..NUM_SQUARES {
-            let u_val = idx(&u, i, j) - 0.5 * (idx(&p, i+1, j) - idx(&p, i-1, j))/h;
-            set_idx(&mut u, u_val, i, j);
+impl Fluid {
+    fn new(visc: f32) -> Self {
+        Self {
+            visc,
 
-            let v_val = idx(&v, i, j) - 0.5 * (idx(&p, 1, j+1) - idx(&p, i, j-1))/h;
-            set_idx(&mut v, v_val, i, j);
+            s: [0f32; ARRAY_SIZE],
+
+            d: [0f32; ARRAY_SIZE],
+            u: [0f32; ARRAY_SIZE],
+            v: [0f32; ARRAY_SIZE],
+
+            d0: [0f32; ARRAY_SIZE],
+            u0: [0f32; ARRAY_SIZE],
+            v0: [0f32; ARRAY_SIZE],
+
+            _div: [0f32; ARRAY_SIZE],
+            _p: [0f32; ARRAY_SIZE],
         }
     }
-    set_bnd(1, &mut u);
-    set_bnd(2, &mut v);
 
-    (u, v)
+    fn project(&mut self) {//u: &mut [f32; ARRAY_SIZE], v: &mut [f32; ARRAY_SIZE], p: &mut [f32; ARRAY_SIZE], div: &mut [f32; ARRAY_SIZE]) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
+        let h = 1./NUM_SQUARES as f32;
+        //let mut div = [0f32; ARRAY_SIZE];
+        //let mut p = [0f32; ARRAY_SIZE];
+    
+        for i in 1..=NUM_SQUARES {
+            for j in 1..=NUM_SQUARES {
+                let div_val = -0.5 * h * (self.u.idx(i+1, j) - self.u.idx(i-1, j) + self.v.idx(i, j+1) - self.v.idx(i, j-1));
+                self._div.set_idx(div_val, i, j);
+                self._p.set_idx(0f32, i, j);
+            }
+        }
+        self._div.set_bnd(0);
+        self._p.set_bnd(0);
+    
+        for _ in 0..20 {
+            for i in 1..NUM_SQUARES {
+                for j in 1..NUM_SQUARES {
+                    let p_val = (self._div.idx(i, j) + self._p.idx(i-1, j) + self._p.idx(i+1, j) + self._p.idx( i, j-1) + self._p.idx( i, j+1))/4.;
+                    self._p.set_idx(p_val, i, j);
+                }
+            }
+            self._p.set_bnd(0);
+        }
+    
+        for i in 1..NUM_SQUARES {
+            for j in 1..NUM_SQUARES {
+                let u_val = self.u.idx(i, j) - 0.5 * (self._p.idx( i+1, j) - self._p.idx(i-1, j))/h;
+                self.u.set_idx(u_val, i, j);
+    
+                let v_val = self.v.idx(i, j) - 0.5 * (self._p.idx(1, j+1) - self._p.idx( i, j-1))/h;
+                self.v.set_idx(v_val, i, j);
+            }
+        }
+        self.u.set_bnd(1);
+        self.v.set_bnd(2);
+    }
+
+    fn dens_step(&mut self, dt: f32) {//mut x: [f32; ARRAY_SIZE], mut x0: [f32; ARRAY_SIZE], u: [f32; ARRAY_SIZE], v: [f32; ARRAY_SIZE], diff: f32, dt: f32) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
+    
+        /*let temp = x;
+        x = x0;
+        x0 = temp;*/
+        std::mem::swap(&mut self.d, &mut self.d0);
+    
+        self.d.diffuse(0, &self.d0, self.visc, dt);
+        
+        /*let temp = x;
+        x = x0;
+        x0 = temp;*/
+        std::mem::swap(&mut self.d, &mut self.d0);
+        
+        self.d.advect(0, &self.d0, &self.u, &self.v, dt);
+    }
+    
+    fn vel_step(&mut self, dt: f32) {//mut u: [f32; ARRAY_SIZE], mut v: [f32; ARRAY_SIZE], mut u0: [f32; ARRAY_SIZE], mut v0: [f32; ARRAY_SIZE], visc: f32, dt: f32 ) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
+        /*let temp = u;
+        u = u0;
+        u0 = temp;*/
+        std::mem::swap(&mut self.u, &mut self.u0);
+
+        self.u.diffuse(1, &self.u0, self.visc, dt);
+    
+        /*let temp = v;
+        v = v0;
+        v0 = temp;*/
+        std::mem::swap(&mut self.v, &mut self.v0);
+        self.v.diffuse(2, &self.v0, self.visc, dt);
+    
+        self.project();
+    
+        /*let temp = u;
+        u = u0;
+        u0 = temp;*/
+        std::mem::swap(&mut self.u, &mut self.u0);
+        
+    
+        /*let temp = v;
+        v = v0;
+        v0 = temp;*/
+        std::mem::swap(&mut self.v, &mut self.v0);
+    
+        self.u.advect(1, &self.u0, &self.u0, &self.v0, 0.01);
+        self.v.advect(2, &self.v0, &self.u0, &self.v0, 0.01);
+    
+        self.project();
+    }
+
+    fn step_simulation(&mut self, dt: f32) {
+        let density_added = 20.;
+        
+        // Add fluid
+        self.d.set_idx(density_added, NUM_SQUARES / 2, NUM_SQUARES / 2);
+        self.d0.set_idx(density_added, NUM_SQUARES / 2, NUM_SQUARES / 2);
+
+        // Velocity step
+        self.vel_step(dt);
+
+        // Density step
+        self.dens_step(dt);
+    }
 }
-
-fn dens_step(mut x: [f32; ARRAY_SIZE], mut x0: [f32; ARRAY_SIZE], u: [f32; ARRAY_SIZE], v: [f32; ARRAY_SIZE], diff: f32, dt: f32) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
-    
-    let temp = x;
-    x = x0;
-    x0 = temp;
-
-    (x, x0) = diffuse(0, x, x0, diff, dt);
-    
-    let temp = x;
-    x = x0;
-    x0 = temp;
-    
-    (x, x0) = advect(0, x, x0, &u, &v, dt);
-    
-    (x, x0, u, v)
-}
-
-fn vel_step(mut u: [f32; ARRAY_SIZE], mut v: [f32; ARRAY_SIZE], mut u0: [f32; ARRAY_SIZE], mut v0: [f32; ARRAY_SIZE], visc: f32, dt: f32 ) -> ([f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE], [f32; ARRAY_SIZE]) {
-    let temp = u;
-    u = u0;
-    u0 = temp;
-    (u, u0) = diffuse(1, u, u0, visc, dt);
-
-    let temp = v;
-    v = v0;
-    v0 = temp;
-    (v, v0) = diffuse(2, v, v0, visc, dt);
-
-    (u, v) = project(u, v);
-
-    let temp = u;
-    u = u0;
-    u0 = temp;
-
-    let temp = v;
-    v = v0;
-    v0 = temp;
-
-    (u, u0) = advect(1, u, u0, &u0, &v0, 0.01);
-    (v, v0) = advect(2, v, v0, &u0, &v0, 0.01);
-
-    (u, v) = project(u, v);
-
-    (u, v, u0, v0)
-}
-
-/*fn wheel(mut pos: u8) -> (u8, u8, u8) {
-    pos = !pos;
-    //(r >> 16) | g | (b << 16);
-    if pos < 85 {
-        return (255 - pos * 3 , 0, pos * 3);
-    }
-    if pos < 170 {
-        pos -= 85;
-        return (0, pos * 3 , 255 - pos * 3);
-    }
-    pos -= 170;
-    
-    (pos * 3, 255 - pos * 3, 0)
-}*/
-
-/*fn color_smoke(d: u8, offset: f32) -> (u8, u8, u8) {
-    let h = d as f32 + offset;
-    let s = 1.0;
-    let v = 1.0;
-
-    let c = s*v;
-    let x = c*(1.-((h/60.) % 2.).abs());
-    let m = v - c;
-
-    let mut r = 0.;
-    let mut g = 0.;
-    let mut b = 0.;
-    if h >= 0. && h < 60. {
-        r = c;
-        g = x;
-        b = 0.;
-    }
-    else if h >= 60. && h < 120. {
-        r = x;
-        g = c;
-        b = 0.;
-    }
-    else if h >= 120. && h < 180. {
-        r = 0.;
-        g = c;
-        b = x;
-    }
-    else if h >= 180. && h < 240. {
-        r = 0.;
-        g = x;
-        b = c;
-    }
-    else if h >= 240. && h < 300. {
-        r = x;
-        g = 0.;
-        b = c;
-    }
-    else{
-        r = c;
-        g = 0.;
-        b = x;
-    }
-
-    
-    r *= h / 127.;
-    g *= h / 127.;
-    b *= h / 127.;
-    
-
-    (((r+m) * 255.) as u8, ((g+m) * 255.) as u8, ((b+m) * 255.) as u8)
-}*/
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -259,15 +255,6 @@ fn main() {
         .software()
         .build()
         .map_err(|e| e.to_string()).unwrap();
-        
-    
-    let mut u_fluid: [f32; ARRAY_SIZE] = [0f32; ARRAY_SIZE];
-    let mut v_fluid: [f32; ARRAY_SIZE] = [0f32; ARRAY_SIZE];
-    let mut d_fluid: [f32; ARRAY_SIZE] = [0f32; ARRAY_SIZE];
-
-    let mut u_prev_fluid: [f32; ARRAY_SIZE] = [0f32; ARRAY_SIZE];
-    let mut v_prev_fluid: [f32; ARRAY_SIZE] = [0f32; ARRAY_SIZE];
-    let mut d_prev_fluid: [f32; ARRAY_SIZE] = [0f32; ARRAY_SIZE];
 
     /*
     for i in 1..NUM_SQUARES {
@@ -275,8 +262,10 @@ fn main() {
     }
     */
     let density_added: f32 = 25.;
-    let visc = 0.01;
+    let visc = 0.005;
     let dt = 1./144.;
+
+    let mut fluid = Fluid::new(visc);
 
     let mut adding = false;
 
@@ -295,39 +284,30 @@ fn main() {
                     adding = false;
                 },
                 Event::MouseMotion { x, y, xrel, yrel, ..} => {
-                    let scale = 50.;
+                    let scale = 10.0;
                     let x = x as usize;
                     let y = y as usize;
                     
                     if adding {
-                        set_idx(&mut u_fluid, scale * xrel as f32,x / GRID_SIZE as usize, y / GRID_SIZE as usize);
-                        set_idx(&mut v_fluid, scale * yrel as f32, x / GRID_SIZE as usize, y  / GRID_SIZE as usize);
+                        fluid.u.set_idx(scale * xrel as f32,x / GRID_SIZE as usize, y / GRID_SIZE as usize);
+                        fluid.v.set_idx(scale * yrel as f32, x / GRID_SIZE as usize, y  / GRID_SIZE as usize);
 
-                        set_idx(&mut u_prev_fluid, scale * xrel as f32, x / GRID_SIZE as usize, y / GRID_SIZE as usize);
-                        set_idx(&mut v_prev_fluid, scale * yrel as f32, x / GRID_SIZE as usize, y / GRID_SIZE as usize);
+                        fluid.u0.set_idx(scale * xrel as f32, x / GRID_SIZE as usize, y / GRID_SIZE as usize);
+                        fluid.v0.set_idx(scale * yrel as f32, x / GRID_SIZE as usize, y / GRID_SIZE as usize);
                     }
                 }
                 _ => {}
             }
         }        
 
-        // Add fluid
-        set_idx(&mut d_fluid, density_added, NUM_SQUARES / 2, NUM_SQUARES / 2);
-        set_idx(&mut d_prev_fluid, density_added, NUM_SQUARES / 2, NUM_SQUARES / 2);
-
-        // Velocity step
-        (u_fluid, v_fluid, u_prev_fluid, v_prev_fluid) = vel_step(u_fluid, v_fluid, u_prev_fluid, v_prev_fluid, visc, dt);
-
-        // Density step
-        (d_fluid, d_prev_fluid, u_fluid, v_fluid) = dens_step(d_fluid, d_prev_fluid, u_fluid, v_fluid, visc, dt);
-        
+        fluid.step_simulation(dt);
 
         
         canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
         canvas.clear();
         for i in 0..NUM_SQUARES {
             for j in 0..NUM_SQUARES {
-                let density = idx(&d_fluid, i, j);
+                let density = fluid.d.idx( i, j);
                 let r = 0;
                 let g = (255. * density).clamp(0., 255.) as u8;
                 let b = (75. * density).clamp(0., 255.) as u8;
